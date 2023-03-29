24607ba9a8b9087e2212c8d3ab1ffbf513fecf79
Senadeera NK <naduni.20191206@iit.ac.lk>
2023-03-29 09:09:04 +0530

    2023/03/29

--- app.py
+++ app.py
@@ -5,6 +5,23 @@
 
 # user should be able to download deepfake audios and real audios seperately
 
+#-- before proprocessing code
+   # # load the audio file
+   # signal, sr = librosa.load(audio_path, sr=16000)
+
+   # #trim the silence from the start and end of the audio
+   # signal,_ = librosa.effects.trim(signal)
+
+   # # extract features using mel spectogram
+   # spectogram = librosa.feature.melspectrogram(signal,sr=8000,n_mels=128)
+   # log_mel_spectrogram = librosa.amplitude_to_db(spectogram, ref=np.max)
+
+   # # normalize the spectogram
+   # normalized_spectogram = (log_mel_spectrogram+80) / 8.0
+
+   # add an additional dimension to the spectogram for the model input
+   #return normalized_spectogram.reshape(1,128,173,1)
+
 from flask import Flask, render_template, request,redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 
@@ -24,6 +41,10 @@ from scipy.ndimage.filters import gaussian_filter
 
 import librosa
 
+#to convert to .wav
+import tempfile
+from scipy.io import wavfile
+
 
 #sys.path.append('D:\\CS FINAL YEAR\\Final Project\\my project\\robust-deepfake-audio-detection\\venv\\Lib\\site-packages')
 
@@ -38,33 +59,25 @@ model = load_model(model_path)
 
 #define a function to preprocess the audio file before feeding it to the model
 def preprocess_audio(audio_path):
-   # # load the audio file
-   # signal, sr = librosa.load(audio_path, sr=16000)
+    print('this is preprocess')
+    with tempfile.NamedTemporaryFile(suffix='.wav') as wav_file:
+        os.system(f"ffmpeg -i {audio_path} -ar 44100 -ac 2 -f wav {wav_file.name}")
+        audio_path = wav_file.name
 
-   # #trim the silence from the start and end of the audio
-   # signal,_ = librosa.effects.trim(signal)
+        #load the audio file
+        audio, sr = librosa.load(audio_path, sr=None)
 
-   # # extract features using mel spectogram
-   # spectogram = librosa.feature.melspectrogram(signal,sr=8000,n_mels=128)
-   # log_mel_spectrogram = librosa.amplitude_to_db(spectogram, ref=np.max)
+        #preprocess the audio file
+        spectrogram = librosa.feature.melspectrogram(y=audio,sr=sr)
+        spectrogram = librosa.power_to_db(spectrogram)
+        spectrogram = np.array(spectrogram, dtype=np.float32)
+        spectrogram_resized = resize(spectrogram, (128,128))
+        spectrogram_resized = gaussian_filter(spectrogram_resized,sigma=(5,5), mode='constant')
+        spectrogram_resized = np.repeat(spectrogram_resized[:,:,np.newaxis], 3, axis=-1)
+        spectrogram_resized = spectrogram_resized / 255.0
 
-   # # normalize the spectogram
-   # normalized_spectogram = (log_mel_spectrogram+80) / 8.0
+        return spectrogram_resized
 
-   # add an additional dimension to the spectogram for the model input
-   #return normalized_spectogram.reshape(1,128,173,1)
-
-
-   #audio_file = [os.path.join(audio_path, f) for f in os.listdir(audio_path) if f.endswith('.wav')]
-   audio, sr = librosa.load(audio_path, sr=None)
-   spectrogram = librosa.feature.melspectrogram(y=audio,sr=sr)
-   spectrogram = librosa.power_to_db(spectrogram)
-   spectrogram = np.array(spectrogram, dtype=np.float32)
-   spectrogram_resized = resize(spectrogram, (128,128))
-   spectrogram_resized = gaussian_filter(spectrogram_resized,sigma=(5,5), mode='constant')
-   spectrogram_resized = np.repeat(spectrogram_resized[:,:,np.newaxis], 3, axis=-1)
-   spectrogram_resized = spectrogram_resized / 255.0
-   return spectrogram_resized
 
 # FLASK APPLICATION BEGINS HERE
 app = Flask(__name__)
